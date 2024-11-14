f7019e9af49f7f10a72a025e17bd79bc60be06ca
rookedsysc <rookedsysc36@gmail.com>
2024-11-14 18:07:36 +0900

    project init

---
+++ src/main/kotlin/org/rookedsysc/rokypay/RokyPayApplication.kt
@@ -0,0 +1,11 @@
+package org.rookedsysc.rokypay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RokyPayApplication
+
+fun main(args: Array<String>) {
+    runApplication<RokyPayApplication>(*args)
+}
---
+++ src/test/kotlin/org/rookedsysc/rokypay/RokyPayApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.rookedsysc.rokypay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RokyPayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
