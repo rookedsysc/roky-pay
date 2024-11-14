b9c72ea2c7d29c7c3d0194f589dc583eff5c226e
rookedsysc <rookedsysc36@gmail.com>
2024-11-15 00:56:40 +0900

    feat: membership-service 생성

---
+++ membership-service/src/main/kotlin/org/rookedsysc/membershipservice/MembershipServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.rookedsysc.membershipservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MembershipServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<MembershipServiceApplication>(*args)
+}
