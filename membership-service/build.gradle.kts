c116dc0d894daf99fc95d2e2798b19f47c9562dd
rookedsysc <rookedsysc36@gmail.com>
2024-11-18 17:16:19 +0900

    config: e2e test를 위한 h2database 세팅 및 application.yaml 작성

--- membership-service/build.gradle.kts
+++ membership-service/build.gradle.kts
@@ -10,7 +10,11 @@ dependencies {
     implementation("org.flywaydb:flyway-mysql:10.21.0")
     implementation("org.flywaydb:flyway-core:10.21.0")
 
+    testImplementation("io.mockk:mockk:1.13.11")
+
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+    testImplementation("com.h2database:h2:2.3.232")
 }
 
-tasks.register("prepareKotlinBuildScriptModel"){}
+tasks.register("prepareKotlinBuildScriptModel") {}
